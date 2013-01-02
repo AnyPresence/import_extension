9445dfd8684dd232e0fd4ff450530bb5b9f17dcc
Khem Veasna <kveasna@anypresence.com>
2013-01-02 18:39:39 -0500

    Added some tests.

--- test/unit/importer_extension/file_import_test.rb
+++ test/unit/importer_extension/file_import_test.rb
@@ -2,9 +2,25 @@ require 'test_helper'
 
 module ImporterExtension
   class FileImportTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    
+    test "import spreadsheet" do
+      ::V1::Outage.any_instance.expects(:save!).times(2)
+      
+      f = Tempfile.new("spreadsheet")
+      f.stubs(:original_filename).returns("#{f.path}.csv")
+      f.write("name,description\ntest0,desc0\ntest1,desc1\n")
+      file_import = FileImport.new
+      f.close
+      file_import.send(:import_spreadsheet, f, ::V1::Outage)
+      f.unlink
+    end
+    
+    test "save without callbacks" do 
+      outage = ::V1::Outage.new
+      outage.expects(:__sms_on_save_perform).never
+      file_import = FileImport.new
+      file_import.send(:save_object_without_callbacks, outage)
+    end
     
   end
 end
