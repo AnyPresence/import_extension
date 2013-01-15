40ab19852552c03ec555f1347b9a453f0ed5f191
Khem Veasna <kveasna@anypresence.com>
2013-01-15 17:39:20 -0500

    Try it again...

--- app/assets/javascripts/importer_extensions/file_imports.js
+++ app/assets/javascripts/importer_extensions/file_imports.js
@@ -1,12 +1,4 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(function () {
-  $('#file').live('change', function(){
-    $in=$(this); 
-    $.post("/api/importer_extension/file_imports/check_file", {file: $in.val()}, function(data) {
-      $('#xml').html(data);
-    });
-  });
-});
 
