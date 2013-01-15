e40e561cda2e54b662c1fada26143735875b7c21
Khem Veasna <kveasna@anypresence.com>
2013-01-15 17:28:30 -0500

    Fix url in js to check file.

--- app/assets/javascripts/importer_extensions/file_imports.js
+++ app/assets/javascripts/importer_extensions/file_imports.js
@@ -1,3 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+$(function () {
+  $('#file').live('change', function(){
+    $in=$(this); 
+    $.post("/api/importer_extension/file_imports/check_file", {file: $in.val()}, function(data) {
+      $('#xml').html(data);
+    });
+  });
+});
+
