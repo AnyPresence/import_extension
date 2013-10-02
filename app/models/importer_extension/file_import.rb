2fa07bf7c4a87ec0b282e6a32fd52c41d412a330
Rick Snyder <rsnyder@anypresence.com>
2013-10-02 10:37:37 -0400

    Change Roo::Csv to Roo:CSV, per suggestion from deprecation warning in Roo library

--- app/models/importer_extension/file_import.rb
+++ app/models/importer_extension/file_import.rb
@@ -94,7 +94,7 @@ module ImporterExtension
   
     def open_spreadsheet(file)
       case File.extname(filename)
-      when ".csv" then ::Roo::Csv.new(file.path, { file_warning: :ignore })
+      when ".csv" then ::Roo::CSV.new(file.path, { file_warning: :ignore })
       when ".xls" then ::Roo::Excel.new(file.path, { file_warning: :ignore })
       when ".xlsx" then ::Roo::Excelx.new(file.path, { file_warning: :ignore })
       else raise "unknown file type: #{filename}"
