473c6e2c326d45f52fdf67e98bd55d9483fa5b15
Khem Veasna <kveasna@anypresence.com>
2013-10-07 10:32:36 -0400

    Fix issue with scanning for latest version of models.

--- config/initializers/extensions.rb
+++ config/initializers/extensions.rb
@@ -3,7 +3,8 @@ Dir.glob(Rails.root.join("app", "models", "*")).each do |path|
   idx = path.rindex(/\//)
   if idx
     file_portion = path.slice(idx+1, path.length)
-    versions << $1 if file_portion.match(/v(\d+)/)
+    versions << $1.to_i if file_portion.match(/v(\d+)/)
   end
 end
+
 ::AP::ImporterExtension::Importer::Config.instance.latest_version = "V#{versions.sort.last}"
